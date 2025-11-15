c0c82ec20a499c019a74b168fc06b522ec0cf8da
hifas86 <hifas86@gmail.com>
2025-11-15 09:07:05 +0300

    Update urls.py
    Add live notification system

--- gold_trade/urls.py
+++ gold_trade/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('goldtrade.urls')),   # include app-level routes
+    path("live-notifications/", views.live_notifications, name="live_notifications"),
 ]
