7630fa31966c915c2ad37ef98ab0d6356bf73244
hifas86 <hifas86@gmail.com>
2025-11-15 10:31:40 +0300

    Update urls.py

--- gold_trade/urls.py
+++ gold_trade/urls.py
@@ -3,5 +3,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('goldtrade.urls')),   # include app-level routes
-    path("live-notifications/", views.live_notifications, name="live_notifications"),
 ]
