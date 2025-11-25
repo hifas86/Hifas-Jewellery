c2bb83ebbc6e27ca3810d8315afe0eb9b1df1b65
Muhamed Hifas Zubair <hifas86@gmail.com>
2025-11-25 05:52:51 +0300

    Restore missing migration 0011 + fix 0012 dependency

---
+++ goldtrade/migrations/0011_auto_20251121_1359.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('goldtrade', '0010_alter_bankdeposit_user_alter_goldrate_buy_rate_and_more'),
+    ]
+
+    operations = [
+    ]
